2282194ba3cc553ba3208a44fb427606167238f5
dev-tom <phucthor369@gmail.com>
2024-12-22 17:01:35 +0700

    03-training using reactive

---
+++ 03-reactivity/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const object = reactive({
+  count: 2,
+  user: {
+    age: 30
+  },
+  array: [1, 2, 3]
+})
+
+const change = () => {
+  object.count++
+  object.user.age++
+  object.array.push(5)
+}
+
+
+</script>
+
+<template>
+  <div>
+    <p>Count: {{ object.count }}</p>
+    <p>Age: {{ object.user.age }}</p>
+    <p>Array: {{ object.array }}</p>
+    <button @click="change">Increase</button>
+  </div>
+</template>
