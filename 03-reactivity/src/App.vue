2e0d95a55bfbd22e3547c5caed46dfee820e6d39
dev-tom <phucthor369@gmail.com>
2024-12-25 14:10:10 +0700

    03

--- 03-reactivity/src/App.vue
+++ 03-reactivity/src/App.vue
@@ -1,28 +1,31 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { nextTick, ref } from 'vue'
 
-const object = reactive({
-  count: 2,
-  user: {
-    age: 30
-  },
-  array: [1, 2, 3]
-})
+// const obj = ref({
+//   nested: { count: 0 },
+//   arr: ['foo', 'bar']
+// })
 
-const change = () => {
-  object.count++
-  object.user.age++
-  object.array.push(5)
+const count = ref(0)
+
+// function mutateDeeply() {
+//   // these will work as expected.
+//   obj.value.nested.count++
+//   obj.value.arr.push('baz')
+// }
+
+const increase = async() => {
+  count.value++
+  console.log(document.getElementById('count').innerText)
+  await nextTick()
+  console.log(document.getElementById('count').innerText)
 }
-
 
 </script>
 
 <template>
   <div>
-    <p>Count: {{ object.count }}</p>
-    <p>Age: {{ object.user.age }}</p>
-    <p>Array: {{ object.array }}</p>
-    <button @click="change">Increase</button>
+    <p id="count">Count: {{ count }}</p>
+    <button @click="increase">INC</button>
   </div>
 </template>
