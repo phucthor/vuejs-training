727dcb329925b23418d7109a285c641c166e745f
dev-tom <phucthor369@gmail.com>
2024-12-26 08:49:22 +0700

    basic computed

---
+++ 04-computed/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, reactive } from 'vue'
+
+const book = reactive({
+  name: 'Book 1',
+  chapters: [],
+})
+
+const addChapter = () => {
+  book.chapters.push('Chapter')
+}
+
+const isPublished = computed(() => {
+  return book.chapters.length > 0 ? 'Yes' : 'No';
+})
+</script>
+
+<template>
+  <div>
+    <p id="count">Book Name: {{ book.name }}</p>
+    <p id="count">Released: {{ isPublished }}</p>
+    <button @click="addChapter">Add Chapter</button>
+  </div>
+</template>
