9b4bfc486a63af0874d656ccc032b1f59bbb2c40
dev-tom <phucthor369@gmail.com>
2024-12-26 16:08:29 +0700

    binding class using multiple way - object, ternary operator

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -1,53 +1,65 @@
 <script setup>
-  import { computed, ref, reactive } from 'vue';
-  const isActive = ref(true)
-  const hasError = ref(true)
+import { computed, ref, reactive } from 'vue'
+const isActive = ref(true)
+const hasError = ref(true)
+const errorClass = ref('has-error')
 
-  // const classObject = reactive({
-  //   active: true,
-  //   'has-error': true
-  // })
+// const classObject = reactive({
+//   active: true,
+//   'has-error': true
+// })
 
-  const classObject = computed(() => ({
-    active: isActive.value && !hasError.value,
-    'has-error': hasError.value
-  }))
-  
+const classObject = computed(() => ({
+  active: isActive.value && !hasError.value,
+  'has-error': hasError.value,
+}))
+
+const changeActive = () => {
+  isActive.value = !isActive.value
+}
 </script>
 
 <template>
   <div>
-    <button class="button" :class="classObject">Active</button>
+    <button @click="changeActive" class="button" :class="classObject">
+      {{ isActive ? 'Active' : 'Deactive' }}
+    </button>
+    <button
+      @click="changeActive"
+      class="button"
+      :class="[isActive ? 'active' : '', { 'has-error': hasError }]"
+    >
+      {{ isActive ? 'Active' : 'Deactive' }}
+    </button>
   </div>
 </template>
 
 <style>
-
-  .button {
-    border: none;
-    background: rgb(230, 192, 205);
-    color: rgb(207, 88, 88);
-    padding: 10px 14px;
-    border-radius: 6px;
-    cursor: pointer;
-    &:hover {
-      background: rgb(230, 224, 225);
-    }
+.button {
+  border: none;
+  background: rgb(136, 0, 48);
+  color: rgb(243, 234, 234);
+  padding: 10px 14px;
+  border-radius: 6px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(245, 82, 109);
   }
+}
 
-  .active {
-    background: rgb(208, 94, 253);
-    color: white;
-    &:hover {
-      background: rgb(174, 103, 202);
-    }
+.active {
+  background: rgb(208, 94, 253);
+  color: white;
+  &:hover {
+    background: rgb(174, 103, 202);
   }
+}
 
-  .has-error {
-    background: rgb(255, 0, 85);
-    color: rgb(255, 255, 255);
-    &:hover {
-      background: rgb(228, 57, 86);
-    }
+.has-error {
+  background: rgb(255, 0, 85);
+  color: rgb(255, 255, 255);
+  &:hover {
+    background: rgb(228, 57, 86);
   }
+}
 </style>
