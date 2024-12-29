4e60acf72859a0fe0db00400753526fefb39ecf0
Nguyen Phuc Tho <phucthor369@gmail.com>
2024-12-29 15:08:32 +0700

    binding style

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -1,65 +1,26 @@
 <script setup>
 import { computed, ref, reactive } from 'vue'
-const isActive = ref(true)
-const hasError = ref(true)
-const errorClass = ref('has-error')
 
-// const classObject = reactive({
-//   active: true,
-//   'has-error': true
-// })
+const color = ref('red')
+const background = ref('blue')
+const fontSize = ref(30)
 
-const classObject = computed(() => ({
-  active: isActive.value && !hasError.value,
-  'has-error': hasError.value,
-}))
-
-const changeActive = () => {
-  isActive.value = !isActive.value
-}
+const styleObject = reactive({
+  border: 'none',
+  borderRadius: '6px'
+})
 </script>
 
 <template>
   <div>
-    <button @click="changeActive" class="button" :class="classObject">
-      {{ isActive ? 'Active' : 'Deactive' }}
-    </button>
     <button
-      @click="changeActive"
-      class="button"
-      :class="[isActive ? 'active' : '', { 'has-error': hasError }]"
+      :style="[{
+        color,
+        background,
+        fontSize: fontSize + 'px',
+      }, styleObject]"
     >
-      {{ isActive ? 'Active' : 'Deactive' }}
+      Button
     </button>
   </div>
 </template>
-
-<style>
-.button {
-  border: none;
-  background: rgb(136, 0, 48);
-  color: rgb(243, 234, 234);
-  padding: 10px 14px;
-  border-radius: 6px;
-  cursor: pointer;
-  &:hover {
-    background: rgb(245, 82, 109);
-  }
-}
-
-.active {
-  background: rgb(208, 94, 253);
-  color: white;
-  &:hover {
-    background: rgb(174, 103, 202);
-  }
-}
-
-.has-error {
-  background: rgb(255, 0, 85);
-  color: rgb(255, 255, 255);
-  &:hover {
-    background: rgb(228, 57, 86);
-  }
-}
-</style>
