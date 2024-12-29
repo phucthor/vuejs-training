c9b5d02dddd4c04c9668a2f785d04d0185cc1ccd
Nguyen Phuc Tho <phucthor369@gmail.com>
2024-12-29 16:17:50 +0700

    v-if, v-show

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -1,26 +1,41 @@
 <script setup>
 import { computed, ref, reactive } from 'vue'
+import { useRoute } from 'vue-router'
+const isLoggedIn = ref(true)
+const toggleLogin = () => {
+  isLoggedIn.value = !isLoggedIn.value
+}
+const userStatus = ref('online')
+const changeStatus = () => {
+  if (userStatus.value === 'online') {
+    userStatus.value = 'away'
+  }else if  (userStatus.value === 'away') {
+    userStatus.value = 'busy'
+  }else if (userStatus.value === 'busy') {
+    userStatus.value = 'online'
+  }
+}
 
-const color = ref('red')
-const background = ref('blue')
-const fontSize = ref(30)
-
-const styleObject = reactive({
-  border: 'none',
-  borderRadius: '6px'
-})
+const isVisible = ref(true);
+const toggleShow = () => {
+  isVisible.value = !isVisible.value
+}
 </script>
 
 <template>
   <div>
-    <button
-      :style="[{
-        color,
-        background,
-        fontSize: fontSize + 'px',
-      }, styleObject]"
-    >
-      Button
-    </button>
+   <h1 v-if="isLoggedIn">Hello logged in user</h1>
+   <h1 v-else>Hello new commer</h1>
+   <button @click="toggleLogin">{{ isLoggedIn ? 'Log out' : 'Log in' }}</button>
+   <br/>
+   <h1 v-if="userStatus === 'online'">User is online</h1>
+   <h1 v-else-if="userStatus === 'away'">User is away</h1>
+   <h1 v-else-if="userStatus === 'busy'">User is busy</h1>
+   <h1 v-else>No status</h1>
+   <button @click="changeStatus">Change Status</button>
   </div>
+  <br>
+  <h1 v-show="isVisible">Hidden or Showing</h1>
+  <button @click="toggleShow">{{ toggleShow ? 'Show' : 'Hidden'}}</button>
+
 </template>
