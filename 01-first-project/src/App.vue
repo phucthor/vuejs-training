7c3ca31982410436433b577a578035610976a7b5
dev-tom <phucthor369@gmail.com>
2025-03-17 17:07:07 +0700

    refs function

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -1,24 +1,38 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-const inputRef = ref(null)
 const list = ref(['apple', 'banana', 'cherry'])
 const itemRefs = ref([])
 onMounted(() => {
-  inputRef.value.focus()
+  console.log(itemRefs.value)
   itemRefs.value.forEach((item) => {
     item.style.transition = 'opacity 3s'
     item.style.opacity = 1
   })
 })
+
+const initializeThirdPartyLibrary = (el) => {
+  console.log('Hello:', el)
+}
 </script>
 
 <template>
   <div>
     <h1>Template refs</h1>
-    <input ref="inputRef" />
+    <input
+      :ref="
+        (el) => {
+          initializeThirdPartyLibrary(el)
+        }
+      "
+    />
 
     <ul>
-      <li v-for="(item, index) in list" :key="index" ref="itemRefs" style="opacity: 0">
+      <li
+        v-for="(item, index) in list"
+        :key="index"
+        :ref="(el) => (itemRefs[index] = el)"
+        style="opacity: 0"
+      >
         {{ item }}
       </li>
     </ul>
