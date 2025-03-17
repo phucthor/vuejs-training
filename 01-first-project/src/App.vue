bf731d6ec925326d92796d2cd5c26a92de79769a
dev-tom <phucthor369@gmail.com>
2025-03-17 16:25:52 +0700

    v-for

--- 01-first-project/src/App.vue
+++ 01-first-project/src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import { reactive, ref, watch } from 'vue'
-
-const x = ref(0)
-const y = ref(0)
-
-const user = reactive({
-  age: 30
+import { onMounted, ref } from 'vue'
+const inputRef = ref(null)
+const list = ref(['apple', 'banana', 'cherry'])
+const itemRefs = ref([])
+onMounted(() => {
+  inputRef.value.focus()
+  itemRefs.value.forEach((item) => {
+    item.style.transition = 'opacity 3s'
+    item.style.opacity = 1
+  })
 })
-
-watch(()=> x.value + y.value,
-(sum) => {
-  // console.log(`Sum of x and y is ${sum}`)
-})
-
-watch([x, () => y.value + 1], ([newX, newY]) => {
-  console.log(`x is ${newX}, y is ${newY}`)
-})
-
-watch(user.age, (newAge) => {
-  console.log(`Age is ${newAge}`)
-})
-
-const increment = () => {
-  x.value++;
-  y.value++;
-}
-
-const changeAge = () => {
-  user.age++;
-}
 </script>
 
 <template>
   <div>
-    <!-- <h1 id="count">Count {{ count }}</h1>
-    <button @click="count++">Increment</button> -->
-    <h1>Watcher</h1>
-    <p>Ask a Yes/No Question</p>
-    <button @click="increment">Increment</button>
-    <p>{{ x }}, {{ y }}</p>
-    <button @click="changeAge">Change Age</button>
-    <p>{{ user.age }}</p>
+    <h1>Template refs</h1>
+    <input ref="inputRef" />
 
-
+    <ul>
+      <li v-for="(item, index) in list" :key="index" ref="itemRefs" style="opacity: 0">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
